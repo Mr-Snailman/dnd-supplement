c51e1944cc9dc88415c743c11c0bd03c9620ec41
Dustin Saunders <dsaunders2414@gmail.com>
2015-05-25 21:37:59 -0400

    Added test to see which is better.

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -5,27 +5,16 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-
-  DiceRoller d4(4);
-  DiceRoller d6(6);
-  DiceRoller d8(8);
-  DiceRoller d10(10);
-  DiceRoller d12(12);
-  DiceRoller d20(20);
-  DiceRoller percent(100);
-  srand(time(NULL));
-
-  for (int i = 0; i < 4; i++) {
-    cout << "d4:" << d4.rollDice() << endl;
-  }
   
-  cout << STRENGTH << endl;
-  cout << WISDOM << endl;
-
+  srand(time(NULL));
+  
   Abilities abs;
 
-  abs = rollAbilities();
+  abs = rollAbilities(rollTylerStyle);
+  cout << "Loaded Char:" << abs << endl;
 
-  cout << abs << endl;
+  abs = rollAbilities(rollNormal);
+  cout << "Normal: " << abs << endl;
+
   return 0;
 }
